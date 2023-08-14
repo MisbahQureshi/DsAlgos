097796a06a17a2e160d1591721162d3cddcfb3a1
Misbah Qureshi <106266858+MisbahQureshi@users.noreply.github.com>
2023-08-14 21:34:35 +0500

    sievePrime

---
+++ Seive.java
@@ -0,0 +1,27 @@
+
+public class Seive {
+
+	public static void main(String[] args) {
+		int n=40;
+		boolean[]arr = new boolean[n+1];
+		seive(n,arr);
+
+	}
+	
+	static void seive(int n, boolean[] arr) {
+		for (int i = 2; i*i <= n; i++) {
+			if(!arr[i]) {
+				for (int j = 2*i; j <=n; j+=i) {
+					arr[j]=true;
+				}
+			}
+		}
+		
+		for (int i = 2; i <= n; i++) {
+			if(!arr[i]) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
